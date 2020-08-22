50cdcf32f6f4745e1d3ae9677739d4d9b68d9ae4
JGabrielSilva <jose.silva225@aluno.ce.gov.br>
2020-08-22 14:46:30 -0300

    finish day 1

--- Manual_kivy/programs.py
+++ Manual_kivy/programs.py
@@ -1,38 +1,22 @@
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ListProperty
-
-class CustomBtn(Widget):
-    pressed = ListProperty([0, 0])
-
-    def on_touch_down(self, touch):
-        if self.collide_point(*touch.pos):
-            self.pressed = touch.pos
-            return True
-        return super(CustomBtn, self).on_touch_down(touch)
-    
-    def on_pressed(self, instance, pos):
-        print(f'pressed at {pos}')
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
 
 
-class RootWidget(BoxLayout):
-    def __init__(self, **kwargs):
-        super(RootWidget, self).__init__(**kwargs)
-        self.add_widget(Button(text='btn 1'))
-        cb = CustomBtn() 
-        cb.bind(pressed=self.btn_pressed)
-        self.add_widget(cb)
-        self.add_widget(Button(text='btn 2'))
-    
-    def btn_pressed(self, instance, pos):
-        print(f'pos: printed from root widget: {pos}')
 
-
-class TestApp(App):
+class Myapp(App):
     def build(self):
-        return RootWidget()
+        layout = BoxLayout()
+        bt = Button(text='Olá, mundo')
+        layout.add_widget(bt)
+        return layout
 
-if __name__ == "__main__":
-    TestApp().run()
+def main():
+    Myapp().run()
+
+
+if __name__ == '__main__':
+    main()
